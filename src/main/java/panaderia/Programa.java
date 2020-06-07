b77f4af24665b78ece278b17ff794e8cef19d707
s0cr4t3sd <smr.jamc@gmail.com>
2020-06-07 16:57:54 +0200

    Guardar, para el video

--- src/main/java/panaderia/Programa.java
+++ src/main/java/panaderia/Programa.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
  */
 public class Programa {
     public static void main(String[] args) {
+        //Para el video
         Pedidos p = new Pedidos();
        
         Bolsa bolsa1 = p.bolsa1();
